67699de984b7f6cd5c9582443e8b5ab8f3290343
Massimo Menichinelli <info@openp2pdesign.org>
2013-11-30 19:02:14 +0100

    Remove panel and scald and add smtp

--- fabdrupal/sites/all/modules/scald/modules/library/dnd/plugins/ckeditor/lang/en.js
+++
@@ -1,8 +0,0 @@
-CKEDITOR.plugins.setLang('dnd', 'en', {
-  atom_properties: 'Edit atom properties',
-  atom_cut: 'Cut atom',
-  atom_paste: 'Paste atom',
-  atom_delete: 'Delete atom from textarea',
-  atom_none: 'Please select an atom first',
-  link_image_only: 'This option is currently available for Image Atoms only.'
-});
